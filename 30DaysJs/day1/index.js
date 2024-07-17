7cf5c55a18aa04650666782f30acc3a80d35c093
Basit <basitmanzoor438@gmail.com>
2024-07-17 18:34:52 +0530

    uploading day1 of my js practice

---
+++ 30DaysJs/day1/index.js
@@ -0,0 +1,54 @@
+//Activity 1
+
+// task 1
+var num = 7;
+console.log(num);
+
+// task2
+let str = "string";
+console.log(str);
+
+// activity2
+
+// task3
+
+const islogged =true;
+console.log(islogged);
+
+// activity3
+
+// task4
+
+let numm =9;
+let basit ="string";
+let isworking = true;
+let myProfile ={
+    folowers: 98,
+    isprivate:true,
+    username:"Basit"
+}
+let marks =[45,34,47];
+
+console.log(typeof numm);
+console.log(typeof basit);
+console.log(typeof isworking);
+console.log(typeof myProfile);
+console.log(typeof marks);
+
+//Activity 4
+let varaible  =0;
+
+console.log(varaible);
+
+varaible=7;
+console.log(varaible);
+
+//activuty 5
+
+// const tryvar = 7;
+
+// tryvar = "newval";
+
+// console.log(tryvar);  this h-gives error as we cannot reassign value ta variable declared using const keyword
+
+
